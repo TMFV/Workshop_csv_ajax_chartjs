55de39dfecc3b1826f0bd199fc6c0bb36c336594
TMFV <68297974+TMFV@users.noreply.github.com>
2021-03-16 22:10:51 +0200

    chart

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
+const ctx = document.querySelector('.js-chart').getContext('2d');
+/* console.log(ctx);
 function fetchData() {
     fetch("./csv/Plumetop_heights_timeseries.csv")
         .then((response) => {return response.text();})
-            .then((data)=>{console.log(data);})
+        .then((data) => {
+            console.log(
+            window.Papa.parse(data, {header: true}).data);
+        })
     
 }
 fetchData();
+console.log(window);
+ */
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
